2fccfc3e2521ba10b5569a68c9bef15464f903e9
Muhydeen Adelani <139554441+DataNaija@users.noreply.github.com>
2023-08-06 02:27:53 -0600

    Update dy_41.test.py

--- dy_41.test.py
+++ dy_41.test.py
@@ -1,8 +1,4 @@
-# Instructions
-# You are going to write a virtual coin toss program. # It will randomly tell the user "Heads" or "Tails".
-# the first letter should be capitalised  and spelt exactly like in the example e.g. Heads, not heads. 
-# you should generate a random number, either 0 or 1.Then use that number to print out Heads or Tails. 
-# e.g. 1 means Heads, 0 means Tails 
+ 
 import random
 test_seed = int(input("Enter the seed number of your choice: "))
 random.seed(test_seed)
